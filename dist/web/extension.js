982ca1509c6afc104979901c05ecb8d39e1173a8
tmpmachine <bill.yosafatz@gmail.com>
2025-03-07 16:15:05 +0700

    fix: fail to check .divless folder

--- dist/web/extension.js
+++ dist/web/extension.js
@@ -6,121 +6,41 @@
 const vscode = __webpack_require__(1);
 const divless = __webpack_require__(2);
 
-async function fileExists(uri) {
-	try {
-		await vscode.workspace.fs.stat(uri);
-		return true;
-	} catch {
-		return false;
-	}
-}
-
-async function createDirectory(uri) {
-	await vscode.workspace.fs.createDirectory(uri);
-}
-
 async function writeFile(uri, content) {
 	const encoder = new TextEncoder();
 	await vscode.workspace.fs.writeFile(uri, encoder.encode(content));
-}
-
-async function readFile(uri) {
-	const decoder = new TextDecoder();
-	const data = await vscode.workspace.fs.readFile(uri);
-	return decoder.decode(data);
 }
 
 function getFileName(uri) {
 	return uri.path.split('/').pop();
 }
 
-function getFileExtension(uri) {
-	return getFileName(uri).split('.').pop();
-}
-
 function activate(context) {
-	let activeCommandListener = vscode.commands.registerCommand(
-		'divlesshtml.createDivlessHTMLFile',
-		async function () {
-			const editor = vscode.window.activeTextEditor;
-			if (!editor) {
-				vscode.window.showErrorMessage('No active editor');
-				return;
-			}
-
-			const document = editor.document;
-			const currentFileUri = document.uri;
-			const currentFileDir = vscode.Uri.joinPath(currentFileUri, '..');
-			const currentFileName = getFileName(currentFileUri);
-			const currentExt = getFileExtension(currentFileUri);
-
-			const divlessDirUri = vscode.Uri.joinPath(currentFileDir, '.divless');
-			const divlessFileUri = vscode.Uri.joinPath(
-				divlessDirUri,
-				currentFileName
-			);
-
-			if (!(await fileExists(divlessDirUri))) {
-				await createDirectory(divlessDirUri);
-			}
-
-			if (!(await fileExists(divlessFileUri))) {
-				const content = document.getText();
-				await writeFile(divlessFileUri, content);
-			}
-
-			const doc = await vscode.workspace.openTextDocument(divlessFileUri);
-			vscode.window.showTextDocument(doc);
-		}
-	);
-
-	const openFileListener = vscode.workspace.onDidOpenTextDocument(
-		async (event) => {
-			const currentExt = getFileExtension(event.uri);
-			if (event.fileName.includes(`.divless.${currentExt}`)) {
-				return;
-			}
-
-			const currentFileUri = event.uri;
-			const currentFileDir = vscode.Uri.joinPath(currentFileUri, '..');
-			const currentFileName = getFileName(currentFileUri);
-			const divlessFileUri = vscode.Uri.joinPath(
-				currentFileDir,
-				'.divless',
-				currentFileName
-			);
-
-			if (await fileExists(divlessFileUri)) {
-				const openFileAction = 'Open divless File';
-				vscode.window
-					.showInformationMessage(
-						'A divless version of this file exists. Avoid making changes to this file.',
-						openFileAction
-					)
-					.then((selection) => {
-						if (selection === openFileAction) {
-							vscode.commands.executeCommand('vscode.open', divlessFileUri);
-						}
-					});
-			}
-		}
-	);
-
 	const saveListener = vscode.workspace.onDidSaveTextDocument(async (event) => {
 		try {
-			const currentExt = getFileExtension(event.uri);
+			// Ensure the file is one of the allowed types
+			const allowedExtensions = ['.html', '.htm', '.razor', '.vue', '.xml'];
+			const fileExt = event.fileName.split('.').pop();
+			if (!allowedExtensions.includes(`.${fileExt}`)) {
+				return; // Exit if not an allowed file type
+			}
 
 			const editor = vscode.window.activeTextEditor;
 			if (!editor) {
-				vscode.window.showErrorMessage('No active editor');
 				return;
 			}
 
 			const document = editor.document;
 			const currentFileUri = document.uri;
 			const currentFileDir = vscode.Uri.joinPath(currentFileUri, '..');
+			const currentDirUri = vscode.Uri.joinPath(currentFileDir);
 			const parentDirUri = vscode.Uri.joinPath(currentFileDir, '..');
 			const currentFileName = getFileName(currentFileUri);
+			const parentDirName = currentDirUri.path.split('/').pop();
+
+			if (parentDirName !== '.divless') {
+				return; // Exit if not inside ".divless"
+			}
 
 			const finalUri = vscode.Uri.joinPath(parentDirUri, currentFileName);
 			const content = event.getText();
@@ -130,8 +50,6 @@ function activate(context) {
 		}
 	});
 
-	context.subscriptions.push(activeCommandListener);
-	context.subscriptions.push(openFileListener);
 	context.subscriptions.push(saveListener);
 }
 
